54911c7d9389cbdcf603e7073bdc47c12f803e02
Vasenin <romanvasenin@yandex.ru>
2025-10-17 22:49:09 +0300

    fix: delete context

--- backend/src/lib/http/fastify.ts
+++ backend/src/lib/http/fastify.ts
@@ -20,36 +20,7 @@ export class FastifyTransport {
     this.#app = fastify({
       logger: this.#config.logger,
     })
-    this.#setupContextMiddleware();
     this.handler = new Handler(this.#app)
-  }
-
-  #setupContextMiddleware() {
-    this.#app.addHook('onRequest', async (request: FastifyRequest, reply: FastifyReply) => {
-      (request as any).context = {
-        user: await this.#getUserFromRequest(request)
-      };
-    });
-  }
-
-  async #getUserFromRequest(request: FastifyRequest): Promise<any> {
-    const token = request.headers.authorization?.replace('JWT ', '');
-    if (!token) {
-      return null;
-    }
-
-    const decoded = await verifyToken(token);
-    
-    // Получаем пользователя из БД по userId из токена
-    const user = await getUser({guid: decoded.userGuid });
-    
-    if (!user) {
-      return null;
-    }
-
-    // Возвращаем пользователя без пароля
-    const { password, ...userWithoutPassword } = user;
-    return userWithoutPassword;
   }
 
   async startServer() {
