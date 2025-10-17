736b75ae5518a523880b396e42cdc339acb10c8f
Vasenin <romanvasenin@yandex.ru>
2025-10-17 12:44:06 +0300

    fix: small fix in route

--- backend/src/app/development/handlers/list.ts
+++ backend/src/app/development/handlers/list.ts
@@ -2,7 +2,7 @@ import { httpTransport } from "#infrastructure/fastify";
 import DevelopmentPlanListSchema from '#app/development/schemas/list';
 import getDevelopmentPlanList from '#app/development/repositories/list';
 
-httpTransport.handler.get('/api/development-plan/v1/list', DevelopmentPlanListSchema, async (req) => {
+httpTransport.handler.get('/api/development/v1/list', DevelopmentPlanListSchema, async (req) => {
   const { userGuid } = req.query;
 
   const developmentPlans = await getDevelopmentPlanList({ userGuid });
